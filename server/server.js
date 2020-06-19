65e42a2a249654341240186be0860e6fb6c2d737
Ryan Geddes <rgeddes09@gmail.com>
2020-06-18 21:31:55 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//dotenv, express cors
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+//THIS IS MAGIC
+console.log(process.env.PORT);
