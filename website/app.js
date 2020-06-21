e7a14e7c5e77e20964381ba91dcfdd796226d3f6
Ryan Geddes <rgeddes09@gmail.com>
2020-06-21 15:20:41 -0700

    moved package json file to root

--- website/app.js
+++ website/app.js
@@ -1,22 +1,20 @@
+'use strict';
 
 // Event Listener on the search button
 $('form').on('submit', getLocation);
 
 function getLocation(e) {
     e.preventDefault();
-    
     //1 get the word(city) user searches for
     
-    let city = $('city-name').val;
+    let city = $('#city-name').val();
     console.log('you searched for', city);
     
     // 2 $.ajax() to our server, asking for location and sending the city
-    //http://http://localhost:3000/location?city=seattle
-    $.ajax(`http://localhost:3000/location?city=${city}`)
+    //`http://localhost:3000/location?city=${city}`
+    $.ajax('http://localhost:3000/location')
         .then (locationData => {
             console.log('ajax pulled from server', locationData);
-        
-        
         
         // 3. render the map using renderMap(lat, long) (or just obj)
         
@@ -35,8 +33,29 @@ function renderMap(location){
     $('#map').html(mapHTML);
 }
 
-function renderHeading();
+function renderHeading(location){
+    let template = $('#heading-template').html();
+    let headingHTML = Mustache.render(template, location);
+    $('#heading').html(headingHTML);
+}
 
-function getRestauarants();
+function getRestauarants(location){
+//$.ajax call
+    $.ajax('http://localhost:3000/restaurants')
+        .then(restaurants =>{
+        console.log(restaurants);
+        renderRestaurants(restaurants);
+        }
+)};
 
-function renderRestaurants();
+function renderRestaurants(restaurants){
+    // take the list and render them with Mustache
+    let $template = $('#restaurants-template').html();
+    let $target = $('#restaurants');
+    $target.empty();
+    restaurants.forEach((r) =>{
+        let rHTML = Mustache.render($template, r);
+        $target.append(rHTML);
+    });
+
+}
