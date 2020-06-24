6bb20accee6610c45b58b9f217afb30a30ada172
Ryan Geddes <rgeddes09@gmail.com>
2020-06-23 18:21:27 -0700

    added require superagent

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 //THIS IS MAGIC
 const PORT = process.env.PORT;
